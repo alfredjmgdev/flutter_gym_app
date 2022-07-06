62b79a87413108c132c9a1ea8202da2bbfaf0ce3
alfred-tera <amujica@teravisiontech.com>
2022-07-06 16:29:41 -0400

    first update

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/bmi_screen.dart';
+import 'package:flutter_application_1/screens/intro_screen.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(GlobeApp());
+}
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        routes: {
+          "/": (context) => IntroScreen(),
+          "/bmi": (context) => BmiScreen()
+        },
+        initialRoute: "/");
+  }
+}
---
+++ lib/screens/intro_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/shared%20/menu_drawer.dart';
+import '../shared /menu_bottom.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text("App de test")),
+          drawer: MenuDrawer(),
+          bottomNavigationBar: MenuBottom(),
+          body: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage("assets/forest.jpg"),
+                fit: BoxFit.cover,
+              )),
+              child: Center(
+                  child: Container(
+                      padding: EdgeInsets.all(24),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color: Colors.white70),
+                      child: Text(
+                        "Testo es una priueba!!!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 22,
+                          shadows: [
+                            Shadow(
+                                offset: Offset(1.0, 1.0),
+                                blurRadius: 2.0,
+                                color: Colors.green)
+                          ],
+                        ),
+                      ))))),
+    );
+    ;
+  }
+}
