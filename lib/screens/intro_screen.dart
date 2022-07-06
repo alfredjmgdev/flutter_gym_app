968260afd2193992a61c55900edc7d733c8af236
alfred-tera <amujica@teravisiontech.com>
2022-07-06 17:34:20 -0400

    fixing navigation

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -7,36 +7,35 @@ class IntroScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(title: Text("App de test")),
-          drawer: MenuDrawer(),
-          bottomNavigationBar: MenuBottom(),
-          body: Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                image: AssetImage("assets/forest.jpg"),
-                fit: BoxFit.cover,
-              )),
-              child: Center(
-                  child: Container(
-                      padding: EdgeInsets.all(24),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(20)),
-                          color: Colors.white70),
-                      child: Text(
-                        "Testo es una priueba!!!",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 22,
-                          shadows: [
-                            Shadow(
-                                offset: Offset(1.0, 1.0),
-                                blurRadius: 2.0,
-                                color: Colors.green)
-                          ],
-                        ),
-                      ))))),
+    return Scaffold(
+      appBar: AppBar(title: Text("App de test")),
+      drawer: MenuDrawer(),
+      bottomNavigationBar: MenuBottom(),
+      body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("assets/forest.jpg"),
+            fit: BoxFit.cover,
+          )),
+          child: Center(
+              child: Container(
+                  padding: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white70),
+                  child: Text(
+                    "Testo es una priueba!!!",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 22,
+                      shadows: [
+                        Shadow(
+                            offset: Offset(1.0, 1.0),
+                            blurRadius: 2.0,
+                            color: Colors.green)
+                      ],
+                    ),
+                  )))),
     );
     ;
   }
